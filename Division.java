25a072563a62aa4298b301e0ad99166caeaf181f
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2023-02-01 11:49:18 +0530

    import java.util.*; class BMW {     public static void main(String args[])     {         int n,m,s;         Scanner sc = new Scanner(System.in);         n = sc.nextInt();         m = sc.nextInt();         s = n/m;         System.out.println(s);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class BMW
+{
+    public static void main(String args[])
+    {
+        int n,m,s;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        s = n/m;
+        System.out.println(s);
+    }
+}

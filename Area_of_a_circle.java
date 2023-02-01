a111d207dd47083c28496e1b55fb2aa6ebfd2151
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2023-02-01 12:06:19 +0530

    import java.util.*; class NJ {     public static void main(String args[])     {         int r;         double p = 3.14,a;         Scanner sc = new Scanner(System.in);         r = sc.nextInt();         a = p*r*r;         System.out.format("%.2f",a);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class NJ
+{
+    public static void main(String args[])
+    {
+        int r;
+        double p = 3.14,a;
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextInt();
+        a = p*r*r;
+        System.out.format("%.2f",a);
+        
+    }
+}
